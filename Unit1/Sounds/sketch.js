b3bd9c67a55867c4491b48ecd38d981e9247ed78
Davidzenge23 <Davidzenge23@gmail.com>
2021-09-30 10:02:02 -0500

    Update sketch.js
    SoundNew

--- Unit1/Sounds/sketch.js
+++ Unit1/Sounds/sketch.js
@@ -1,67 +1,26 @@
-let state = 0
-let song1, song2;
-
+let song1 ;
 function preload() {
   song1 = loadSound("assets/jazz.mp3");
-  song2 = loadSound("assets/ukulele.mp3");
-
-
-
-  song1.loop();
-  song2.loop();
-
-  song1.pause();
-  song2.pause();
-
-
 }
-
 function setup() {
   createCanvas(500, 500);
+  song1.loop();
 }
 
 function draw() {
-  switch (state) {
-    case 0:
-      background('grey');
-      song1.play();
-      state = 1;
-      break;
-
-    case 1:
-      background('grey');
-      break;
-
-    case 2:
-      song2.play();
-      state = 3;
-
-      break;
-
-    case 3:
-      background("orange");
-      break;
-
+  createCanvas(500, 500);
+  background('orange')
 
   }
 
-  fill('Blue');s
-  rect(100, 100, 100, 100);
-
-
-}
 
 function mouseReleased() {
-  if ((mouseX > 100) && (mouseX < 200) && (mouseY > 100) && (mouseY < 200)) {
-
-
-    song1.pause();
-    song2.pause();
-    state++;
-
-    if (state > 4) {
-      state = 0;
+  if (song1.isPlaying()) {
+    song1.pause();} else {
+      song1.loop();
     }
 
   }
-}
+  function touchStarted() {
+    getAudiocontext().resume();
+  }
