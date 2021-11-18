7675f98c044dee67ce6c593fa3c7e50d2957f8c4
Davidzenge23 <Davidzenge23@gmail.com>
2021-11-18 09:34:49 -0600

    st

--- Unit4/walking/sketch.js
+++ Unit4/walking/sketch.js
@@ -1,13 +1,13 @@
 var num;
 var distance;
-var font1, font2;
+//var font1, font2;
 
 var locationData;
 
 
 function preload() {
   locationData = getCurrentPosition();
-  font1 = loadFont("assets/Mohave-Light.ttf");
+//  font1 = loadFont("assets/Mohave-Light.ttf");
 }
 
 function setup() {
